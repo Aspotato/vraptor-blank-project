978fd92e8d8d4efe53890df87235b641810e440f
ojunge <adronilsonf@gmail.com>
2018-07-18 22:38:38 -0300

    Removido arquivos desnecessários

---
+++ src/br/com/vraptor/controller/IndexController.java
@@ -0,0 +1,20 @@
+package br.com.vraptor.controller;
+
+import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Resource;
+import br.com.caelum.vraptor.Result;
+
+@Resource
+public class IndexController {
+
+	private final Result result;
+
+	public IndexController(Result result) {
+		this.result = result;
+	}
+
+	@Path("/")
+	public void index() throws Exception {
+		result.include("variable", "Teste");
+	}
+}
